95f0e86741aa59b0e03f5f140d4b3ae2e0a37050
jkao2 <163955972+Zerippo@users.noreply.github.com>
2024-11-15 11:40:47 -0600

    Create TreeNode.java

---
+++ TreeNode.java
@@ -0,0 +1,88 @@
+public class TreeNode implements Comparable<TreeNode> {
+
+    /** The data payload of the node */
+    private String word;
+    /** Its left and right pointers */
+    private TreeNode left;
+    private TreeNode right;
+
+    /**
+     * Basic constructor creates a simple node with a payload and two null children.
+     * 
+     * @param word the data we store in the node.
+     */
+    public TreeNode(String word) {
+        this.word = word;
+        this.left = null;
+        this.right = null;
+    } // basic constructor
+
+    /**
+     * Implementation of Comparable using the nodes' content strings as basis for
+     * comparison. If the
+     */
+    public int compareTo(TreeNode other) {
+        int result = 1;
+        if (other != null)
+            result = this.getWord().compareTo(other.getWord());
+        return result;
+    } // method compareTo
+
+    /**
+     * String representation of the TreeNode.
+     * 
+     * For now it just returns TreeNode.word, so it's identical -- in form -- to
+     * getWord(). In future versions we may want to write something more
+     * sophisticated, like the contents of the node along with some information
+     * about its two children.
+     */
+    public String toString() {
+        return this.word;
+    } // method toString
+
+    /** Accesors and mutators */
+
+    public boolean hasLeft() {
+        return this.left != null;
+    }
+
+    public boolean hasRight() {
+        return this.right != null;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public TreeNode getRight() {
+        return this.right;
+    }
+
+    public TreeNode getLeft() {
+        return this.left;
+    }
+
+    public void setLeft(TreeNode node) {
+        this.left = node;
+    }
+
+    public void setRight(TreeNode node) {
+        this.right = node;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public int countChildren() {
+        int count = 0;
+        if (this.hasLeft()) {
+            count++;
+        }
+        if (this.hasRight()) {
+            count++;
+        }
+        return count;
+    }// returns the number of children of a TreeNode
+
+} // class TreeNode
